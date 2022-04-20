fd4a4aa63dd212be18025eb41a03658a446e90ce
Erik Reinert <erik@altf4.email>
2022-04-20 12:03:44 -0700

    fix(cmp): re-add mappings

--- roles/neovim/files/lua/TheAltF4Stream/plugins/cmp.lua
+++ roles/neovim/files/lua/TheAltF4Stream/plugins/cmp.lua
@@ -3,6 +3,34 @@ local function init()
   local lspkind = require'lspkind'
 
   cmp.setup({
+    snippet = {
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+      end,
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    }),
+    sources = {
+      { name = 'cmp_tabnine' },
+      { name = 'nvim_lsp' },
+      { name = 'nvim_lua' },
+      { name = 'treesitter' },
+      { name = 'vsnip' },
+    },
     formatting = {
       format = lspkind.cmp_format({
         maxwidth = 50,
@@ -16,33 +44,11 @@ local function init()
         }),
         with_text = true
       }),
-    },
-    snippet = {
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
-      end,
-    },
-    mapping = {
-      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-      ['<C-e>'] = cmp.mapping({
-        i = cmp.mapping.abort(),
-        c = cmp.mapping.close(),
-      }),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    },
-    sources = {
-      { name = 'cmp_tabnine' },
-      { name = 'nvim_lsp' },
-      { name = 'nvim_lua' },
-      { name = 'treesitter' },
-      { name = 'vsnip' },
     }
   })
 
   cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
     sources = {
       { name = 'buffer' }
     }
