8b49ed40d3eec9c3a8d1e4d8b4a05873f314bb1b
Erik Reinert <erik@altf4.email>
2022-05-08 22:27:14 -0700

    fix(neovim)

--- roles/neovim/files/lua/TheAltF4Stream/plugins/lspconfig.lua
+++ roles/neovim/files/lua/TheAltF4Stream/plugins/lspconfig.lua
@@ -20,16 +20,15 @@ local function on_attach(client, bufnr)
   buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 
-  -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
-    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  if client.server_capabilities.documentFormattingProvider then
+    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
   end
-  if client.resolved_capabilities.document_range_formatting then
+
+  if client.server_capabilities.documentRangeFormattingProvider then
     buf_set_keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
   end
 
-  -- Set autocommands conditional on server_capabilities
-  if client.server_capabilities.document_highlight then
+  if client.server_capabilities.documentHighlightProvider then
     vim.api.nvim_exec([[
       augroup lsp_document_highlight
         autocmd! * <buffer>
